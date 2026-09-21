3bb751f9000f57c33e12caaafac8c98cb52c4078
Joseph Nishimwe <j.nishimwe@alustudent.com>
2026-09-21 11:09:06 +0200

    crypto.h

---
+++ src/crypto.h
@@ -0,0 +1,25 @@
+#ifndef CRYPTO_H
+#define CRYPTO_H
+
+#include <openssl/evp.h>
+#include <stddef.h>
+
+EVP_PKEY *generate_key_pair(void);
+
+int sign_data(
+    EVP_PKEY *private_key,
+    const unsigned char *data,
+    size_t data_len,
+    unsigned char *signature,
+    size_t *signature_len
+);
+
+int verify_signature(
+    EVP_PKEY *public_key,
+    const unsigned char *data,
+    size_t data_len,
+    const unsigned char *signature,
+    size_t signature_len
+);
+
+#endif
